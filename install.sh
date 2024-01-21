5c90d9804934a064a1e8af18abe7f84d030e4861
Eygonn <90767469+Eygonn@users.noreply.github.com>
2024-01-21 13:17:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@
 current_directory=$(dirname "$(readlink -f "$0")")
 
 # Export immédiat
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$current_directory/src
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$current_directory/lib
 
 # Ajouter la ligne au fichier .bashrc
-ld_library_path_line="export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$current_directory/src"
+ld_library_path_line="export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$current_directory/lib"
 if ! grep -q "$ld_library_path_line" "$HOME/.bashrc"; then
     echo "$ld_library_path_line" >> "$HOME/.bashrc"
-    echo "La variable LD_LIBRARY_PATH a été ajoutée au fichier .bashrc avec le chemin $current_directory/src"
+    echo "La variable LD_LIBRARY_PATH a été ajoutée au fichier .bashrc avec le chemin $current_directory/lib"
 else
     echo "La variable LD_LIBRARY_PATH est déjà configurée dans le fichier .bashrc"
 fi
 
 # Afficher un message (facultatif)
-echo "LD_LIBRARY_PATH a été mis à jour avec $current_directory/src"
+echo "LD_LIBRARY_PATH a été mis à jour avec $current_directory/lib"
